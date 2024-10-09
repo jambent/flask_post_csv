125747b40db17635326c1e4c5cd3bb94baf64967
James Bennett <James.Bennett@apexon.com>
2024-10-09 16:04:48 +0100

    Implements Postman-only implementation of posting CSV data, echoing the posted data back in the response

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
 """
-Allows user to select local JSON file for upload, which is then converted to
-a DataFrame, before being converted to a CSV file and uploaded to
-the uploaded_files folder
+Allows user to POST data to /upload endpoint, the data being
+echoed back as a JSON object
 """
 
-import os
-import datetime as dt
-import polars as pl
-from flask import Flask, request, render_template
-from werkzeug.utils import secure_filename
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-
-ALLOWED_EXTENSIONS = "json"
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route("/")
-def home():
-    return "Main page"
 
 
 @app.route("/upload", methods=["GET", "POST"])
@@ -30,22 +13,10 @@ def upload_table():
 
     if request.method == "POST":
 
-        file = request.files["file"]
-        if file and allowed_file(file.filename):
-            
-            transition_df = pl.read_json(file)
+        input_data = request.data
+        input_data_str = input_data.decode('utf-8')
 
-            filename = secure_filename(file.filename)
-            new_filename = f"{
-                filename.split(".")[0]}_{
-                str(
-                    dt.datetime.now().isoformat(
-                        timespec = "hours"))}.csv"
-            save_location = os.path.join("uploaded_files", new_filename)
-
-            transition_df.write_csv(save_location)
-
-    return render_template("upload_csv.html")
+    return jsonify({'post_body': input_data_str})
 
 
 if __name__ == '__main__':
--- utilities/write_test_df_to_json_file.py
+++
@@ -1,5 +0,0 @@
-from test_data.test_dataframe import test_df
-
-
-if __name__ == "__main__":
-    test_df.write_json("test_data/test_df_json.json")
